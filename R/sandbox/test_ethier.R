d8f048adb182ea487283a858ceca24ac7a7e89ca
oshethier <jeth7319@students.vsu.edu>
2025-06-13 11:04:07 -0400

    worked on reading data into tidyverse and utilized testdata

---
+++ R/sandbox/test_ethier.R
@@ -0,0 +1,13 @@
+# Test script on 6/13/25
+
+# load packages
+library(tidyverse)
+
+library(lubridate)
+
+
+#practicing reading files
+read.csv("test_data/MarineGEOBC_bioblitz_specimen_20180403.csv")
+read_csv("test_data/MarineGEOBC_bioblitz_specimen_20180403.csv")
+
+d<-read_csv("test_data/MarineGEOBC_bioblitz_specimen_20180403.csv")
