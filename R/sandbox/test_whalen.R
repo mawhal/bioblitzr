60087b33b5553495a989f10d57ead3f055ed5c17
mawhal <mawhal@gmail.com>
2025-06-16 11:09:04 -0400

    update test code

--- R/sandbox/test_whalen.R
+++ R/sandbox/test_whalen.R
@@ -3,16 +3,14 @@
 
 # load packages
 library(tidyverse)
-library(lubridate)
+# library(lubridate)
 #
 
-
 # practice reading files
-read_csv()
+d = read_csv("test_data/MarineGEOBC_bioblitz_specimen_20180403.csv")
+k = read_csv("test_data/Koz_list.csv")
 
-
-
-# goals for next week
+# goals for week 16 June 2025
 # 1) comparing BioBlitz data to historical baselines (VENN)
 # 2) explore possibilities for visualizations
 # 3) possible with partner - updates on goals
@@ -20,3 +18,39 @@ read_csv()
 
 
 
+# Visualization ideas
+# 1) heatmap
+# 2) Venn diagram
+
+
+
+## Tasks for 16 June 2025
+
+# compare morphospecies in specimen bioblitz data  and Scientificname accepted in kozloff
+d
+class(d)
+# access columns of data.frame using the dollar sign
+dnames = d$`scientificName (morphospecies)`
+knames = k$ScientificName_accepted
+
+# duplicates?
+dnames = unique(dnames) # lots of duplicates
+unique(knames) # all unique
+
+# are the unique species names in d also in k?
+dnames %in% knames
+# TRUE
+# FALSE
+# T
+# F
+T + T
+T + F
+F + F
+sum(dnames %in% knames)
+
+
+
+# how many species from the Kozloff list (k) are in the Bioblitz dataset (d)
+paste( "percent found in Kozloff =", 
+       round( sum(knames %in% dnames) / 
+                length(knames) * 100, 1),"%" )
