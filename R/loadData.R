8ed87929d037acff26d5d6aa6a17f9e3537fc940
mawhal <mawhal@gmail.com>
2025-07-23 14:29:59 -0400

    create loadData script

---
+++ R/loadData.R
@@ -0,0 +1,8 @@
+### code to prepare user-supplied datasets to be used in other scripts
+
+
+
+# Things that must be included
+# 1. species list
+# 2. geolocations
+# 3. comparative dataset with names (optional)
